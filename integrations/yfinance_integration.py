7521aafca9269aaf9b2fef95113674f4f34dc0dc
iRhysBradbury <rhys.alan.bradbury@gmail.com>
2024-10-11 16:20:03 +0100

    add pycache to gitignore

---
+++ integrations/yfinance_integration.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+from pandas import DataFrame
+
+
+def data(tickers: list[str],  start: str, end: str) -> DataFrame:
+    return yf.download(
+        tickers=tickers,
+        start=start,
+        end=end
+    )
